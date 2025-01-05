518fac09209165ba6cb5cda609fed383c7a40cc8
heks <heks@localhost.localdomain>
2025-01-05 22:15:58 +0100

    comments added

--- portscan_echo.sh
+++ portscan_echo.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#Port Scanner with echo on local ports
+
 function ctrl_c(){
   echo -e "\n\n[+] Saliendo .....\n"
   exit 1
 }
-
+#Ctrl+c
 trap ctrl_c INT
 
+#
 for port in $(seq 1 65535); do
   (echo ' ' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo -e "\n[+]Puerto: $port =>abierto\n" || echo -e "\n[+]Puerto: $port=>cerrado\n" &
   sleep 1
