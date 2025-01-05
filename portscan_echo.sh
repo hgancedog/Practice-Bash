c3b43c3a71b792e9e106732d76c0bbf739332be2
heks <heks@localhost.localdomain>
2025-01-05 21:20:22 +0100

    creado y comprobado, funciona

---
+++ portscan_echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function ctrl_c(){
+  echo -e "\n\n[+] Saliendo .....\n"
+  exit 1
+}
+
+trap ctrl_c INT
+
+for port in $(seq 1 65535); do
+  (echo ' ' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo -e "\n[+]Puerto: $port =>abierto\n" || echo -e "\n[+]Puerto: $port=>cerrado\n" &
+  sleep 1
+done
